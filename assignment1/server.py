9ea3c71425df8b0fc70e43322789c6cec79925d7
Ben Cleveland <ben.j.cleveland@gmail.com>
2011-01-12 07:14:26 +0000

    startings of a server

--- assignment1/server.py
+++ assignment1/server.py
@@ -1,1 +1,34 @@
 #!/usr/bin/python
+
+
+import socket
+
+
+def create_listen_socket(host, port):
+    '''
+    Creates a listen socket on the given hostname and port
+    returns the created socket
+    '''
+
+    sock = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
+
+    sock.bind((host,port))
+    sock.listen(5)
+
+    # return the created socket
+    return sock
+
+def main():
+    '''
+    The main funtion for the server
+    '''
+    print 'hello world!'
+
+    # create the listen socket
+    sock = create_listen_socket( socket.gethostname(), 54321 )
+
+    
+
+if __name__  == '__main__':
+    # execute main
+    main()
