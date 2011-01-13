097853bffc37c2a7cd86eb2e0e4c3e1007647f72
Ben Cleveland <ben.j.cleveland@gmail.com>
2011-01-13 05:34:13 +0000

    Added threading and tested out the server a little bit

--- assignment1/server.py
+++ assignment1/server.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python
 
-
 import socket
-
+from threading import Thread
 
 def create_listen_socket(host, port):
     '''
@@ -10,24 +9,48 @@ def create_listen_socket(host, port):
     returns the created socket
     '''
 
+    # create the socket
     sock = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
 
+    # bind to the port
     sock.bind((host,port))
     sock.listen(5)
 
     # return the created socket
     return sock
 
+def handle_connection( conn, addr ):
+    '''
+    handles reading and writing to and from the socket
+    '''
+    print 'in thread',conn, addr
+
+    # do nothing for now
+    while True:
+        pass
+
+    # close the connection when finished
+    conn.close()
+
 def main():
     '''
     The main funtion for the server
     '''
-    print 'hello world!'
+    print 'adding server'
 
     # create the listen socket
     sock = create_listen_socket( socket.gethostname(), 54321 )
 
-    
+    # await for a connection
+    while True:
+        conn, addr = sock.accept()
+        print 'Received connection from ', addr
+
+        # create a thread to handle the connection
+        t = Thread(target = handle_connection, args=(conn,addr)) 
+
+        # start the thread
+        t.start()
 
 if __name__  == '__main__':
     # execute main
