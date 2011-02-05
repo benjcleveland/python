3111e301a88a62baf3e8dbb83cf09ad5c00514a2
Ben Cleveland <ben.j.cleveland@gmail.com>
2011-02-05 23:31:34 +0000

    Put a little error handling into the adder

--- assignment4/adder.py
+++ assignment4/adder.py
@@ -4,9 +4,22 @@ from urlparse import parse_qs
 
 def index( req ):
     
+    #$return req.args
+    # make sure that we have args passed in 
+    if req.args == None:
+        return 'No args'
+
+    # parse the arguments 
     a = parse_qs( req.args )
 
-    res = int(a['a'][0]) + int(a['b'][0])
+    try:
+        # try to add bot the numbers
+        res = int(a['a'][0]) + int(a['b'][0])
+    except:
+        return 'Invalid argument',req.args
+    
+    # create a dictionary with all the information we want to return
     dic = {'time': time.time(), 'result': res, 'uwnetid':'cleveb'}
 
-    return json.dumps( dic ) 
+    # return the data in JSON 
+    return json.dumps( dic, sort_keys=True, indent=4 ) 
