379cee73227381e15d4d54bd6dc174662f05bd5a
Ben Cleveland <ben.j.cleveland@gmail.com>
2011-02-14 03:03:04 +0000

    Cleaned up the book info page

--- assignment5/book_site.py
+++ assignment5/book_site.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python
 
 '''
-    Basic webite using cherrypy.
+    Basic webite using cherrypy.  Uses genshi for html templates.
     Uses the bookdb module for a psuedo backend database
-
 '''
 
 import bookdb
@@ -24,6 +23,7 @@ class booksite(object):
         # get a copy of the database
         self.bdb = bookdb.BookDB()
 
+    @cherrypy.expose
     def index(self):
         '''
         handle the index page
@@ -34,20 +34,24 @@ class booksite(object):
     
         return tmpl.generate(title='Library', links=self.bdb.titles()).render('html', doctype='html') 
 
-    def book(self, id):
+    @cherrypy.expose # equivalent of book.exposed = True
+    def book(self, id=None):
         '''
         handle the book page.
         Will render the book information based on the id that is passed
         '''
 
+        if not bookdb.database.has_key(id):
+            # make sure this key is in the database
+            raise cherrypy.HTTPError(404,'404 Not Found')
+        
+        # get the title info from the database
         title_info = bookdb.BookDB().title_info(id)
 
+        # load the book page template
         tmpl = self.loader.load('book.html')
    
         return tmpl.generate(title='Book Info', book=title_info).render('html', doctype='html') 
-
-    book.exposed = True
-    index.exposed = True
 
 # cherrypy site configuration
 booksite_config  = os.path.join(os.path.dirname(__file__), 'book_site.conf')
