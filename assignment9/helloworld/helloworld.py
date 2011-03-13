bfb9043a25256b6f620c5a01cd36238cce3850a9
Ben Cleveland <ben.j.cleveland@gmail.com>
2011-03-13 22:47:17 +0000

    able to import the postgres database to gae

--- assignment9/helloworld/helloworld.py
+++ assignment9/helloworld/helloworld.py
@@ -1,5 +1,7 @@
 import models
+import datetime
 from google.appengine.ext import webapp
+from google.appengine.ext.bulkload import transform
 from google.appengine.ext.webapp.util import run_wsgi_app
 
 class MainPage(webapp.RequestHandler):
@@ -7,9 +9,37 @@ class MainPage(webapp.RequestHandler):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.out.write('Hello, webapp World!')
 
+        g = models.Game()
+        g.title = 'title'
+        g.publisher = 'publisher'
+        g.year_published = '2001'
+        g.description = 'description'
+        g.last_played = datetime.datetime.now()
+        g.image_name = 'image_name'
+        g.genre = 'genretest'
+        g.maxplayers = 10
+        g.minplayers = 1
+
+        g.put()
+
+        r = models.Rating()
+        r.game = g
+        r.rating = 7.2
+        r.name = 'name'
+        r.comment = 'comment'
+
+        r.put()
+
+        self.response.out.write(dir(transform))
         games = models.Game.all()
         for game in games:
-            self.response.out.write(game.title + '\n')
+#            self.response.out.write(' '.join([game.title, game.publisher,'\n']))
+            self.response.out.write(game.title + game.genre + game.publisher +'\n')
+
+
+        rating = models.Rating.all()
+        for r in rating:
+            self.response.out.write(r.game.title + ' ' + str(r.rating) + ' ' + r.name + ' ' + r.comment + '\n' )
 
 application = webapp.WSGIApplication(
                         [('/', MainPage)],
--- assignment9/helloworld/models.py
+++ assignment9/helloworld/models.py
@@ -3,4 +3,20 @@ from google.appengine.ext import db
 class Game(db.Model):
     title = db.StringProperty()
     publisher = db.StringProperty()
+    year_published = db.StringProperty()
+    description = db.StringProperty()
+    last_played = db.DateTimeProperty()
+    image_name = db.StringProperty()
+    genre = db.StringProperty()
+    maxplayers = db.IntegerProperty()
+    minplayers = db.IntegerProperty()
+    
+
+class Rating(db.Model):
+    game = db.ReferenceProperty(Game)
+    rating = db.FloatProperty()
+    name = db.StringProperty()
+    comment = db.StringProperty(multiline=True)
+
+
 
